5a93fc6cba9deee206dc5c5ee1c20b1b7cecfc8a
Aaro Reijola <aaro.reijola@aalto.fi>
2021-09-13 18:16:00 +0300

    tehtävä 1.3

--- part1/kurssitiedot/src/App.js
+++ part1/kurssitiedot/src/App.js
@@ -19,9 +19,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part = {props.p1} exercises = {props.e1} />
-      <Part part = {props.p2} exercises = {props.e2} />
-      <Part part = {props.p3} exercises = {props.e3} />
+      <Part part = {props.p1.name} exercises = {props.p1.exercises} />
+      <Part part = {props.p2.name} exercises = {props.p2.exercises} />
+      <Part part = {props.p3.name} exercises = {props.p2.exercises} />
     </div>
   )
 }
@@ -36,18 +36,24 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content p1={part1} p2={part2} p3={part3} e1={exercises1} e2={exercises2} e3={exercises3} />
-      <Total e1={exercises1} e2={exercises2} e3={exercises3} />
+      <Content p1={part1} p2={part2} p3={part3} />
+      <Total e1={part1.exercises} e2={part2.exercises} e3={part3.exercises} />
     </div>
   )
   
