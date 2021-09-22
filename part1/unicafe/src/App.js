b4462b9f980dae074df9b58523f906d0100866e3
Aaro Reijola <aaro.reijola@aalto.fi>
2021-09-22 11:15:03 +0300

    tehtävä 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -10,14 +10,28 @@ const Display = ({text, count}) => (
   <p>{text} {count}</p>
 )
 
+const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad
+  const avg = (good - bad) / all
+  
+  return (
+    <div>
+      <Display text = "good" count = {good}/>
+      <Display text = "neutral" count = {neutral}/>
+      <Display text = "bad" count = {bad}/>
+      <Display text = "all" count = {all}/>
+      <Display text = "average" count = {avg}/>
+      <Display text = "positive" count = {(good / all) * 100 + " %"}/>
+    </div>
+  )
+}
+
 
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const all = good + neutral + bad
-  const avg = (good - bad) / all
 
   return (
     <div>
@@ -26,12 +40,7 @@ const App = () => {
       <Button handleClick = {() => setNeutral(neutral + 1)} text = "neutral"/>
       <Button handleClick = {() => setBad(bad + 1)} text = "bad"/>
       <h1>statistics</h1>
-      <Display text = "good" count = {good}/>
-      <Display text = "neutral" count = {neutral}/>
-      <Display text = "bad" count = {bad}/>
-      <Display text = "all" count = {all}/>
-      <Display text = "average" count = {avg}/>
-      <Display text = "positive" count = {(good / all) * 100 + "%"}/>
+      <Statistics good = {good} neutral = {neutral} bad = {bad}/>
     </div>
   )
 }
