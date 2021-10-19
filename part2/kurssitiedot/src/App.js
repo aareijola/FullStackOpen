9fe10b749fb608583bbb0efd8dd70786c85bb82f
Aaro Reijola <aaro.reijola@aalto.fi>
2021-10-19 18:13:23 +0300

    tehtävä 2.4

--- part2/kurssitiedot/src/App.js
+++ part2/kurssitiedot/src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = (props) => {
   return (
     <div>
-      <h1>{props.course.name}</h1>
+      <h2>{props.course.name}</h2>
     </div>
   )
 }
@@ -27,7 +27,7 @@ const Content = (props) => {
 }
 
 const Total = ({parts}) => {
-  const sum = parts.reduce((t,c) => t + c.exercises, 0)
+  const sum = parts.reduce( (t,c) => t + c.exercises, 0)
   return (
     <div>
       <h3> total of {sum} exercises </h3>
@@ -46,31 +46,55 @@ const Course = ({course}) => {
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course} />
+      <h1>Web development curriculum</h1>
+      {courses.map(course => <Course course = {course} key = {course.id}/>)}
     </div>
   )
 }
