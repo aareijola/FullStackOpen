c799669c9bfff024a20e2207fe0efb5223bb94cd
Aaro Reijola <aaro.reijola@aalto.fi>
2021-11-15 13:38:58 +0200

    tehtävä 2.8

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -5,18 +5,25 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber] = useState('')
   
-  const handleChange  = (event) => {
+  const handleNameChange  = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const addNewPerson = (event) => {
     event.preventDefault()
     if (persons.find((person) => person.name === newName) === undefined) {
       const newPersonObject = {
-        name : newName
+        name : newName,
+        number : newNumber
       }
       setNewName("")
+      setNewNumber("")
       setPersons(persons.concat(newPersonObject))
     }
     else {
@@ -24,23 +31,33 @@ const App = () => {
     }
   }
 
-
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit = {addNewPerson}>
         <div>
-          name: <input value = {newName} onChange = {handleChange} />
+          name: <input value = {newName} onChange = {handleNameChange} />
+        </div>
+        <div> 
+          number: <input value = {newNumber} onChange = {handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key = {person.name}>{person.name}</div>)}      
+            <Persons persons = {persons}/>
     </div>
   )
 
+}
+
+const Persons = ({persons}) => {
+  return <div>{ persons.map(person => <Person person = {person} key = {person.name} />) }</div>
+}
+
+const Person = ({person}) => {
+  return <div>{person.name} {person.number}</div>
 }
 
 
