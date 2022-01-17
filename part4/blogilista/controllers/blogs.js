f4ec098bab9870cd911844bf6aff645159a2bf69
Aaro Reijola <aaro.reijola@aalto.fi>
2022-01-17 21:43:44 +0200

    tehtävä 4.19

--- part4/blogilista/controllers/blogs.js
+++ part4/blogilista/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = req => {
+  const auth = req.get('authorization')
+  if (auth && auth.toLowerCase().startsWith('bearer ')) {
+    return auth.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (req, res) => {
   const blogs = await Blog.find({}).populate('user', {username: 1, name: 1, id: 1})
@@ -39,9 +48,12 @@ blogsRouter.put('/:id', async (req, res) => {
 
 blogsRouter.post('/', async (req, res) => {
   const body = req.body
-  const users = await User.find({})
-  const user = users[0]
-  console.log('user:', user)
+  const token = getTokenFrom(req)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return res.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
   const blog = new Blog({
     title: body.title,
     author: body.author,
