b3d4aff20603cccfd6e9fe56d60db7b18242de49
Aaro Reijola <aaro.reijola@aalto.fi>
2022-01-18 18:10:56 +0200

    tehtävä 5.2

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -15,6 +15,7 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
+      window.localStorage.setItem('loggedBlogUser', JSON.stringify(user))
       setUser(user)
       setUsername('')
       setPassword('')
@@ -22,12 +23,25 @@ const App = () => {
       console.log('There will be an a handler but login failed yoo')
     }
   }
+  const handleLogout = async (event) => {
+    window.localStorage.removeItem('loggedBlogUser')
+    setUser(null)
+  }
   
   
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+
+    }
   }, [])
 
   if (!user) {
@@ -64,7 +78,7 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       <div>
-        <p>{user.name} logged in</p>
+        <p>{user.name} logged in<button onClick={handleLogout}>logout</button></p>
       </div>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
