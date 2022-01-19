b2f9ca75769b040ac022946e906d447024a9565b
Aaro Reijola <aaro.reijola@aalto.fi>
2022-01-19 20:51:07 +0200

    tehtävä 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,32 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+const Blog = ({blog}) => {
+  const [showAll, setShowAll] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
 
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} <button onClick={() => setShowAll(!showAll)}>{showAll ? 'hide':'show'}</button>
+      {showAll ?
+       <div>
+         <div>
+           {blog.url}
+         </div>
+         <div>
+           likes {blog.likes} <button>like</button>
+         </div>
+         <div>
+           {blog.user.name}
+         </div>
+       </div>:null}
+    </div>
+    
+  )
+}
+ 
 export default Blog
