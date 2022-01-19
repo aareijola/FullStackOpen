ad8a44a2c4e5fc2ab8f8620200268a0fbe5d32d6
Aaro Reijola <aaro.reijola@aalto.fi>
2022-01-19 20:03:12 +0200

    tehtävä 5.5

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -3,6 +3,7 @@ import Blog from './components/Blog'
 import NewBlogForm from './components/BlogForm'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import Togglable from './components/Togglable'
 import Alerts from './components/Alerts'
 import './index.css'
 
@@ -112,7 +113,9 @@ const App = () => {
       <div>
         <p>{user.name} logged in<button onClick={handleLogout}>logout</button></p>
       </div>
-      <NewBlogForm onSubmit={handleNewBlog}/>
+      <Togglable buttonLabel='Create new blog'>
+        <NewBlogForm onSubmit={handleNewBlog}/>
+      </Togglable>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
