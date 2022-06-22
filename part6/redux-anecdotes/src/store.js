7c99662f091ad17404691a73a5a6d89a2768d2cf
Aaro Reijola <aaro.reijola@aalto.fi>
2022-06-22 14:10:44 +0300

    tehtävä 6.9

---
+++ part6/redux-anecdotes/src/store.js
@@ -0,0 +1,10 @@
+import anecdoteReducer from './reducers/anecdoteReducer'
+import { configureStore } from '@reduxjs/toolkit'
+
+const store = configureStore({
+    reducer: {
+        anecdotes: anecdoteReducer
+    }
+})
+
+export default store
