0dae12e1a005723c0c7765eb55c39dedcb9f7fdb
Aaro Reijola <aaro.reijola@aalto.fi>
2022-07-10 21:10:36 +0300

    exercise 9.17

---
+++ part9/patientor/src/PatientPage/index.tsx
@@ -0,0 +1,62 @@
+import axios from 'axios';
+import { apiBaseUrl } from '../constants';
+import { useParams } from 'react-router-dom';
+import { useStateValue } from '../state';
+import { Patient } from '../types';
+import { Male, Female } from '@mui/icons-material';
+
+const PatientPage = () => {
+  const { id } = useParams<{ id: string }>();
+  const updatePatientData = async (id: string) => {
+    try {
+      const { data: patient } = await axios.get<Patient>(
+        `${apiBaseUrl}/patients/${id}`
+      );
+      dispatch({ type: 'UPDATE_PATIENT', payload: patient });
+    } catch (e: unknown) {
+      if (axios.isAxiosError(e)) {
+        console.error(e?.response?.data || 'Unrecognized axios error');
+      } else {
+        console.error('Unknown error', e);
+      }
+    }
+  };
+
+  if (!id) {
+    return null;
+  }
+  const [{ patients }, dispatch] = useStateValue();
+  const patient = patients[id];
+  if (!patient) {
+    return null;
+  }
+  if (!patient.ssn) {
+    void updatePatientData(id);
+  }
+  let genderIcon = null;
+  switch (patient.gender) {
+    case 'male':
+      genderIcon = <Male />;
+      break;
+    case 'female':
+      genderIcon = <Female />;
+      break;
+    default:
+      genderIcon = null;
+  }
+
+  return (
+    <div>
+      <h2>
+        {patient.name + ' '}
+        {genderIcon}
+      </h2>
+      <p>
+        ssh: {patient.ssn} <br />
+        occupation: {patient.occupation}
+      </p>
+    </div>
+  );
+};
+
+export default PatientPage;
