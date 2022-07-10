9016fd4cf89b91ffc712b69d03b0b694a0afb365
Aaro Reijola <aaro.reijola@aalto.fi>
2022-07-10 17:12:58 +0300

    exercise 9.15

--- part9/ts-app/src/App.tsx
+++ part9/ts-app/src/App.tsx
@@ -1,53 +1,158 @@
-const Header = ({name}: {name: string}) => {
-  return (
-    <h1>{name}</h1>
-  )
+// new types
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  type: string;
+}
+
+interface DescribedCoursePartBase extends CoursePartBase {
+  description: string;
+}
+
+interface CourseNormalPart extends DescribedCoursePartBase {
+  type: 'normal';
+}
+interface CourseProjectPart extends CoursePartBase {
+  type: 'groupProject';
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends DescribedCoursePartBase {
+  type: 'submission';
+  exerciseSubmissionLink: string;
+}
+
+interface CourseSpecialPart extends DescribedCoursePartBase {
+  type: 'special';
+  requirements: Array<string>;
+}
+
+type CoursePart =
+  | CourseNormalPart
+  | CourseProjectPart
+  | CourseSubmissionPart
+  | CourseSpecialPart;
+
+const Header = ({ name }: { name: string }) => {
+  return <h1>{name}</h1>;
 };
 
-type CoursePart = {
-  name: string,
-  exerciseCount: number
-}
+const assertNever = (value: never): never => {
+  throw new Error(`Unhandled union member: ${JSON.stringify(value)}`);
+};
 
-const Content = ({ parts }: { parts: Array<CoursePart>}) => {
+const Part = ({ part }: { part: CoursePart }) => {
+  switch (part.type) {
+    case 'normal':
+      return (
+        <p>
+          <b>
+            {part.name} {part.exerciseCount}
+          </b>
+          <div>
+            <i>{part.description}</i>
+          </div>
+        </p>
+      );
+    case 'groupProject':
+      return (
+        <p>
+          <b>
+            {part.name} {part.exerciseCount}
+          </b>
+          <div>project exercises {part.groupProjectCount}</div>
+        </p>
+      );
+    case 'submission':
+      return (
+        <p>
+          <b>
+            {part.name} {part.exerciseCount}
+          </b>
+          <div>
+            <i>{part.description}</i>
+          </div>
+          <div>submit to {part.exerciseSubmissionLink}</div>
+        </p>
+      );
+    case 'special':
+      return (
+        <p>
+          <b>
+            {part.name} {part.exerciseCount}
+          </b>
+          <div>
+            <i>{part.description}</i>
+          </div>
+          <div>required skills: {part.requirements.join(', ')}</div>
+        </p>
+      );
+    default:
+      return assertNever(part);
+  }
+};
+
+const Content = ({ parts }: { parts: Array<CoursePart> }) => {
   return (
-  <div>
-    {parts.map((p) => (<p key={p.name}>{p.name} {p.exerciseCount}</p>))}
-  </div>)
-}
+    <div>
+      {parts.map((p) => (
+        <Part key={p.name} part={p} />
+      ))}
+    </div>
+  );
+};
 
-const Total = ({ parts }: {parts: Array<CoursePart>}) => {
+const Total = ({ parts }: { parts: Array<CoursePart> }) => {
   return (
     <p>
-      Number of exercises{" "}
+      Number of exercises{' '}
       {parts.reduce((carry, part) => carry + part.exerciseCount, 0)}
     </p>
-    )
-}
-
+  );
+};
 
 const App = () => {
-  const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseName = 'Half Stack application development';
+  const courseParts: CoursePart[] = [
     {
-      name: "Fundamentals",
-      exerciseCount: 10
+      name: 'Fundamentals',
+      exerciseCount: 10,
+      description: 'This is the leisured course part',
+      type: 'normal',
     },
     {
-      name: "Using props to pass data",
-      exerciseCount: 7
+      name: 'Advanced',
+      exerciseCount: 7,
+      description: 'This is the harded course part',
+      type: 'normal',
     },
     {
-      name: "Deeper type usage",
-      exerciseCount: 14
-    }
+      name: 'Using props to pass data',
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      type: 'groupProject',
+    },
+    {
+      name: 'Deeper type usage',
+      exerciseCount: 14,
+      description: 'Confusing description',
+      exerciseSubmissionLink: 'https://fake-exercise-submit.made-up-url.dev',
+      type: 'submission',
+    },
+    {
+      name: 'Backend development',
+      exerciseCount: 21,
+      description: 'Typing the backend',
+      requirements: ['nodejs', 'jest'],
+      type: 'special',
+    },
   ];
 
   return (
     <div>
-      <Header name= {courseName}/>
-      <Content parts={courseParts}/>
-      <Total parts={courseParts}/>
+      <Header name={courseName} />
+      <Content parts={courseParts} />
+      <Total parts={courseParts} />
     </div>
   );
 };
